1d4d151c4c4fbf0379a181046d6f010588c7f860
ShahlaKamal96 <shahla.kamal19@gmail.com>
2023-01-23 00:00:46 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i + 1; j < array.length; j++) {
+      console.log(array[i], array[j])
+      if (array[i] + array[j] === target) {
+        return true
+      }
+    }
+  }
+  return false
 }
+
+
+
 
 /* 
   Write the Big O time complexity of your function here
